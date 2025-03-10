a2469a6354a722cadd79c60851a20fa0a06b6718
김희주 <ajtwlsgmlwn@naver.com>
2025-03-10 23:23:48 +0900

    study

--- study/captain/0310.py
+++ study/captain/0310.py
@@ -91,47 +91,96 @@
 
 
 
-k=int(input()) # 정점의 개수
-n=int(input()) # 간선 정보의 개수
-lst=[list(input().split()) for _ in range(n)]
-lst.sort(key=lambda x:int(x[2]))
-arr=[0]*200
-
-def findboss(m):
-    if not arr[ord(m)]:
-        return m
-    ret=findboss(arr[ord(m)])
-    arr[ord(m)]=ret
-    return ret
-
-def union(a,b,i):
-    global arr,cnt,total_cost
-    a_boss,b_boss=findboss(a),findboss(b)
-    if a_boss==b_boss:
-        return
-    total_cost+=int(lst[i][2])
-    cnt+=1
-    arr[ord(b_boss)]=a_boss
-
-total_cost=0 # 최종 비용
-cnt=0 # 연결되는 간선의 개수
-for i in range(n):
-    if cnt==k-1:
-        print(total_cost)
-        break
-    union(lst[i][0],lst[i][1],i)
-
-
-5
-8
-A B 9
-A C 3
-A E 7
-B C 14
-B D 11
-C D 1
-C E 5
-A D 20
+# k=int(input()) # 정점의 개수
+# n=int(input()) # 간선 정보의 개수
+# lst=[list(input().split()) for _ in range(n)]
+# lst.sort(key=lambda x:int(x[2]))
+# arr=[0]*200
+#
+# def findboss(m):
+#     if not arr[ord(m)]:
+#         return m
+#     ret=findboss(arr[ord(m)])
+#     arr[ord(m)]=ret
+#     return ret
+#
+# def union(a,b,i):
+#     global arr,cnt,total_cost
+#     a_boss,b_boss=findboss(a),findboss(b)
+#     if a_boss==b_boss:
+#         return
+#     total_cost+=int(lst[i][2])
+#     cnt+=1
+#     arr[ord(b_boss)]=a_boss
+#
+# total_cost=0 # 최종 비용
+# cnt=0 # 연결되는 간선의 개수
+# for i in range(n):
+#     if cnt==k-1:
+#         print(total_cost)
+#         break
+#     union(lst[i][0],lst[i][1],i)
 
 
+
+# 5
+# 8
+# A B 9
+# A C 3
+# A E 7
+# B C 14
+# B D 11
+# C D 1
+# C E 5
+# A D 20
+
+
+def find(parent, x):  # 부모 노드를 찾는 함수 (경로 압축)
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, rank, a, b):  # 두 집합을 합치는 함수 (유니온)
+    rootA = find(parent, a)
+    rootB = find(parent, b)
+    if rootA != rootB:
+        if rank[rootA] > rank[rootB]:  # 랭크가 높은 쪽이 부모가 됨
+            parent[rootB] = rootA
+        else:
+            parent[rootA] = rootB
+            if rank[rootA] == rank[rootB]:
+                rank[rootB] += 1
+
+
+def kruskal(n, edges):
+    parent = [i for i in range(n + 1)]  # 부모 테이블
+    rank = [0] * (n + 1)  # 랭크 정보
+    edges.sort(key=lambda x: x[2])  # 가중치 기준 정렬
+
+    mst_cost = 0
+    mst_edges = []
+
+    for u, v, weight in edges:
+        if find(parent, u) != find(parent, v):  # 사이클이 없으면 선택
+            union(parent, rank, u, v)
+            mst_edges.append((u, v, weight))
+            mst_cost += weight
+
+    return mst_cost, mst_edges
+
+
+# 예제 입력: (노드 개수, 간선 리스트)
+n = 4  # 노드 개수
+edges = [
+    (1, 2, 1),
+    (2, 3, 3),
+    (3, 4, 2),
+    (4, 1, 4),
+    (1, 3, 5)
+]
+
+mst_cost, mst_edges = kruskal(n, edges)
+print("최소 신장 트리 비용:", mst_cost)
+print("선택된 간선:", mst_edges)
 
