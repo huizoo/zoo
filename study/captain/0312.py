4c4baa358d7dcd7b8c9db91ddc8b1dc17b47cce5
김희주 <ajtwlsgmlwn@naver.com>
2025-03-13 00:40:00 +0900

    dijkstra

--- study/captain/0312.py
+++ study/captain/0312.py
@@ -111,12 +111,12 @@ heapq.heappush(heap, Node(4, 2))
 #     print(temp.a, temp.b)
 
 
-heap = [(1, 5), (1, 9), (4, 2)]
-heap = list(map(lambda x: Node(x[0], x[1]), heap))
-heapq.heapify(heap)
-while heap:
-    temp = heapq.heappop(heap)
-    print(temp.a, temp.b)   # 정수형으로 출력
+# heap = [(1, 5), (1, 9), (4, 2)]
+# heap = list(map(lambda x: Node(x[0], x[1]), heap))
+# heapq.heapify(heap)
+# while heap:
+#     temp = heapq.heappop(heap)
+#     print(temp.a, temp.b)   # 정수형으로 출력
 
 
 
@@ -127,3 +127,39 @@ while heap:
 2. 시작점이 정해져있어야 함.
 '''
 
+name='ABCDE'
+inf=int(21e8)
+arr=[
+    [0,3,inf,9,5],
+    [inf,0,7,inf,1],
+    [inf,inf,0,1,inf],
+    [inf,inf,inf,0,inf],
+    [inf,inf,1,inf,0]]
+used=[0]*5
+used[0]=1 # A(0번인덱스)를 시작점으로 등록
+result=[0,3,inf,9,5] # 첫번째 경유지를 시작점인 A로 놓았을 경우
+
+def select_ky():
+    Min=int(21e8)
+    Minindex=0
+    for i in range(5):
+        if used[i]==0 and result[i]<Min:
+            Min=result[i]
+            Minindex=i
+    return Minindex
+
+def dijkstra():
+    # 경유지 선택
+    for i in range(4):
+        via=select_ky()
+        used[via]=1
+
+    # 시작->경유
+    # 시작->경유->도착 비교 후 작은 값으로 갱신
+        for j in range(5): # result 배열에 갱신 할 것임
+            baro=result[j]
+            kyoung=result[via]+arr[via][j]
+            if baro>kyoung:
+                result[j]=kyoung
+dijkstra()
+print(*result)
