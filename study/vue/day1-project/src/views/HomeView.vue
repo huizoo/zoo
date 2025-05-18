888815fc28a4b366313188087ffefda5e7b53d01
김희주 <ajtwlsgmlwn@naver.com>
2025-05-19 07:56:49 +0900

    study

---
+++ study/vue/day1-project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Home</h1>
+  <div>message: {{ message }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello Vue')
+
+</script>
+
+<style scoped>
+
+</style>
