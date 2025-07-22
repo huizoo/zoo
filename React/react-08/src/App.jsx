a9076ecf573a24988f878617e46eedf8598aed01
김희주 <ajtwlsgmlwn@naver.com>
2025-07-23 00:57:44 +0900

    home study

---
+++ React/react-08/src/App.jsx
@@ -0,0 +1,11 @@
+import MemoContainer from "./components/MemoContainer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MemoContainer />
+    </div>
+  );
+};
+
+export default App;
